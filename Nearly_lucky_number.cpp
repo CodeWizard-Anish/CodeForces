542d133f793f7e67f8997dfc3c2e91eb6dbf68fb
Anish <anishraj4343@gmail.com>
2026-07-17 19:20:26 +0530

    Updated Nearly Lucky Number

--- Nearly_lucky_number.cpp
+++ Nearly_lucky_number.cpp
@@ -1,20 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int n;
-    cin>>n;
-    int count=0;
-    while(n>0){
-        if(n%10==4 || n%10==7){
+int main() {
+    string s;
+    cin >> s;
+
+    int count = 0;
+
+    for (char ch : s) {
+        if (ch == '4' || ch == '7')
             count++;
-        }
-        n/=10;
     }
-    if(count==4 || count==7){
-        cout<<"YES"<<endl;
-    }
-    else{
-        cout<<"NO"<<endl;
-    }
+
+    if (count == 4 || count == 7)
+        cout << "YES";
+    else
+        cout << "NO";
 }
