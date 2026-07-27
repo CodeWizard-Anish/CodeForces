462fc8f52df79e044ef2aca643e4d102970603ef
Anish <anishraj4343@gmail.com>
2026-07-27 22:20:09 +0530

    Magnets Done

---
+++ Magnets.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int count = 0;
+    int prev = 0;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin>>x;
+        if(x != prev){
+            count++;
+        }
+        prev = x;
+    }
+    cout<<count;
+    return 0;
+}
