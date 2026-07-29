4d37713d4468a2af32e6867a0e8d6a303f9d7312
Anish <anishraj4343@gmail.com>
2026-07-29 18:23:05 +0530

    Presents Done

---
+++ Presents.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int ans[n + 1];
+
+    for (int i = 1; i <= n; i++) {
+        int x;
+        cin >> x;
+        ans[x] = i;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
