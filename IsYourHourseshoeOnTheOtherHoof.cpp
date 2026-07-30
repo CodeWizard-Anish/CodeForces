6e4e84b082bc591b1818043a7a188533607888fd
Anish <anishraj4343@gmail.com>
2026-07-30 17:53:44 +0530

    Is Your HorseShoe On the Other Hoof Done

---
+++ IsYourHourseshoeOnTheOtherHoof.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    set<int> st;
+
+    for (int i = 0; i < 4; i++) {
+        int x;
+        cin >> x;
+        st.insert(x);
+    }
+
+    cout << 4 - st.size();
+
+    return 0;
+}
