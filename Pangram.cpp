caf5ad39c1b420f5490a3a56f4a472deb768820b
Anish <anishraj4343@gmail.com>
2026-08-03 14:12:01 +0530

    Pangram Done

---
+++ Pangram.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<set>
+#include<cctype>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    string s;
+    cin>>s;
+
+    set<char> st;
+
+    for(char c:s){
+        st.insert(tolower(c));
+    }
+    if(st.size() == 26){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+    return 0;
+}
