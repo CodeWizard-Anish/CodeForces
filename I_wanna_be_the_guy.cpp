0d261e09aaabe9cce78eafeb845ac6453ceea280
Anish <anishraj4343@gmail.com>
2026-08-05 19:45:42 +0530

    I wanna be the guy done

---
+++ I_wanna_be_the_guy.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    set<int> st;
+
+    int p;
+    cin >> p;
+    for (int i = 0; i < p; i++) {
+        int x;
+        cin >> x;
+        st.insert(x);
+    }
+
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        int x;
+        cin >> x;
+        st.insert(x);
+    }
+
+    if (st.size() == n)
+        cout << "I become the guy.";
+    else
+        cout << "Oh, my keyboard!";
+
+    return 0;
+}
