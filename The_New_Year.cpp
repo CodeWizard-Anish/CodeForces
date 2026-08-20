184dcbe9e19b9393a51dbe5ad60c098f6ecac31f
Anish <anishraj4343@gmail.com>
2026-08-20 18:42:02 +0530

    The New Year done

---
+++ The_New_Year.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int mn = min(a, min(b, c));
+    int mx = max(a, max(b, c));
+
+    cout << mx - mn << '\n';
+
+    return 0;
+}
