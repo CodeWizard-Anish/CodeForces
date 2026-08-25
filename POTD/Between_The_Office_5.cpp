fc52b9ef06f3203707c3438207dd9901dcb17423
Anish <anishraj4343@gmail.com>
2026-08-25 14:04:04 +0530

    Between the office done

---
+++ POTD/Between_The_Office_5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    string s;
+    cin>>s;
+    
+    if(s[0] == 'S' && s[n-1] == 'F'){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    
+    return 0;
+}
