a428e0b456c4ca03bd8c11d50deffec2243ff5b9
Anish <anishraj4343@gmail.com>
2026-09-05 19:17:40 +0530

    Hossam and Friends done

---
+++ POTD/Hossam_and_Friends62.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    int a,b;
+    vector<int>c(n+1,0);
+    
+    for(int i=0;i<m;i++){
+        cin>>a>>b;
+        
+    }
+    long long ans = 0;
+    int left = 1;
+
+    
+    cout<<ans<<endl;
+}
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        solve();
+    }
+    return 0;
+}
