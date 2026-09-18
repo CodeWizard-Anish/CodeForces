d08ad2c566f0e746b96ecf0760324d697b3d1c41
Anish <anishraj4343@gmail.com>
2026-09-18 22:59:02 +0530

    GCd partition done

---
+++ POTD/GCD_Partiition73.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<ll> a(n);
+
+    ll total = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        total += a[i];
+    }
+
+    ll prefix = 0;
+    ll ans = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        prefix += a[i];
+
+        ans = max(ans, gcd(prefix, total));
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+//gcd(x,S−x)=gcd(x,S)
